a633d0b25e48a296c650525edc94e714bf9b2be1
Julianna Gines <jmgines.12@gmail.com>
2025-05-08 18:23:25 +0800

    Added program outline

--- answer_generator.py
+++ answer_generator.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+
+# Function to load questions from the file
+# Function to show a random question
+# Function to check user's answer
+# GUI Setup
+# Load questions and begin quiz
